7b89df696a55895e0b2965db5ff9077be1f6b948
hellowon11 <jiehao19980808@gmail.com>
2025-09-10 23:32:13 +0800

    Fix nodemailer createTransporter typo

--- api/contact.js
+++ api/contact.js
@@ -31,7 +31,7 @@ app.use(cors({
 }));
 
 // Email transporter configuration
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
